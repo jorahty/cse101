fcdfa2e772ff2755977a6f103e20e87babc89f73
James Tennant <jtennant@ucsc.edu>
2022-03-31 12:29:03 -0700

    Lex & ListTest can be excecuted

--- pa1/Lex.c
+++ pa1/Lex.c
@@ -1,1 +1,10 @@
 // James Tennant • jtennant • pa1
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf("Hello Lex!\n");
+    
+    return 0;
+}
--- pa1/List.c
+++ pa1/List.c
@@ -3,7 +3,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdbool.h>
-#include "Queue.h"
+#include "List.h"
 
 // structs --------------------------------------------------------------------
 
