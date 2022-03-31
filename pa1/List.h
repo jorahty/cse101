84ac33763af34b504b5b987bd5e1b37e9223e623
James Tennant <jtennant@ucsc.edu>
2022-03-31 16:28:13 -0700

    Redesigned List.h with reference to pa1.pdf

--- pa1/List.h
+++ pa1/List.h
@@ -4,58 +4,60 @@
 
 #include <stdbool.h>
 
+// ██ Exported Types ██
 
-// Exported types -------------------------------------------------------------
-typedef int QueueElement;
-typedef struct QueueObj* Queue;
+typedef struct ListObj* List;
 
+// ██ Constructors-Destructors ██
 
-// Constructors-Destructors ---------------------------------------------------
+List newList(); // Creates and returns a new empty List
+void freeList(List* pL); // Frees all heap memory associated with *pL, and sets *pL to NULL
 
-// newQueue()
-// Returns reference to new empty Queue object. 
-Queue newQueue();
+// ██ Access Functions ██
 
-// freeQueue()
-// Frees all heap memory associated with Queue *pQ, and sets *pQ to NULL.
-void freeQueue(Queue* pQ);
+int length(List L); // Returns the number of elements in L
+int index(List L); // Returns index of cursor element if defined, -1 otherwise
+int front(List L); // Returns front element of L. Pre: length() > 0
+int back(List L); // Returns back element of L. Pre: length() > 0
+int get(List L); // Returns cursor element of L. Pre: length() > 0, index() >= 0
+bool equals(List A, List B); // Returns true iff A & B are in same state, false otherwise
 
+// ██ Manipulation Procedures ██
 
-// Access functions -----------------------------------------------------------
+void clear(List L); // Resets L to its original empty state.
+void set(List L, int x); // Overwrites the cursor element’s data with x. Pre: length()>0, index()>=0
+void moveFront(List L); // If L is non-empty, sets cursor under the front element, otherwise does nothing.
+void moveBack(List L); // If L is non-empty, sets cursor under the back element, otherwise does nothing.
 
-// getFront()
-// Returns the value at the front of Q.
-// Pre: !isEmpty(Q)
-QueueElement getFront(Queue Q);
+// If cursor is defined and not at front, move cursor one step toward the front of L;
+// if cursor is defined and at front, cursor becomes undefined; if cursor is undefined do nothing
+void movePrev(List L);
 
-// getLength()
-// Returns the length of Q.
-int getLength(Queue Q);
+// If cursor is defined and not at back, move cursor one step toward the back of L;
+// if cursor is defined and at back, cursor becomes undefined; if cursor is undefined do nothing
+void moveNext(List L);
 
-// isEmpty()
-// Returns true if Q is empty, otherwise returns false.
-bool isEmpty(Queue Q);
+void prepend(List L, int x); // Insert new element into L. If L is non-empty,
+// insertion takes place before front element.
+void append(List L, int x); // Insert new element into L. If L is non-empty,
+// insertion takes place after back element.
 
+void insertBefore(List L, int x); // Insert new element before cursor. Pre: length()>0, index()>=0
+void insertAfter(List L, int x); // Insert new element after cursor. Pre: length()>0, index()>=0
+void deleteFront(List L); // Delete the front element. Pre: length()>0
+void deleteBack(List L); // Delete the back element. Pre: length()>0
+void delete(List L); // Delete cursor element, making cursor undefined. Pre: length()>0, index()>=0
 
-// Manipulation procedures ----------------------------------------------------
+// ██ Other Functions ██
 
-// Enqueue()
-// Places new data at the back of Q.
-void Enqueue(Queue Q, QueueElement data);
+// Prints to the file pointed to by out, a
+// string representation of L consisting
+// of a space separated sequence of integers,
+// with front on left.
+void printList(FILE* out, List L);
 
-// Dequeue()
-// Deletes data at front of Q.
-// Pre: !isEmpty(Q)
-void Dequeue(Queue Q);
-
-
-// Other Functions ------------------------------------------------------------
-
-// printQueue()
-// Prints a string representation of Q consisting of a space separated list 
-// of ints to stdout.
-void printQueue(Queue Q);
-
-// equals()
-// Returns true if A is same int sequence as B, false otherwise.
-bool equals(Queue A, Queue B);
+// Returns a new List representing the same integer
+// sequence as L. The cursor in the new list is undefined,
+// regardless of the state of the cursor in L. The state
+// of L is unchanged.
+List copyList(List L);
