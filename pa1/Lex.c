481da58d892c1da80235c3c07810d8a956181a07
James Tennant <jtennant@ucsc.edu>
2022-04-03 13:15:02 -0700

    Create List of indices that indirectly sorts array

--- pa1/Lex.c
+++ pa1/Lex.c
@@ -1,11 +1,13 @@
 // James Tennant • jtennant • pa1
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "List.h"
 
 #define KB 1024
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
     // Check for correct number of arguments
     if (argc != 3) {
@@ -32,8 +34,68 @@ int main(int argc, char *argv[]) {
     char line[KB];
     while (fgets(line, KB, in) != NULL) { n++; }
 
-    rewind(in);
 
+    // Allocate heap memory for array of strings
+    char** strings = calloc(n, sizeof(char*));
+
+    rewind(in); // Go back to start of input file
+
+    for (int i = 0; i < n; i++) { // Read all n lines again
+
+        // Read each line from input file
+        // and store it in line buffer
+        fgets(line, KB, in);
+
+        // Allocate minimum heap memory for string
+        strings[i] = calloc(strlen(line), sizeof(char)); 
+
+        // Copy string from line buffer to array
+        strcpy(strings[i], line);
+
+        // Remove newline
+        // strings[i][strlen(strings[i]) - 1] = '\0';
+        strings[i] = strtok(strings[i], "\n");
+    }
+    printf("             ");
+    for (int i = 0; i < n; i++) { printf("%d    ", i); }
+    printf("\n");
+    printf("strings = ( ");
+    for (int i = 0; i < n; i++) { printf("\"%s\", ", strings[i]); }
+    printf(")\n\n");
+
+
+    // Create List of indices that indirectly sorts array
+    List L = newList();
+    append(L, 0);
+    for (int i = 1; i < n; i++) { // For every index i in (0, n)
+
+        // Scan for a place to insert i
+        for (moveFront(L); 1; moveNext(L)) {
+
+            // Have we reached the end of the List?
+            if (index(L) == -1) {
+                append(L, i);
+                break;
+            }
+
+            // Does this index i belong before the cursor element?
+            if (strcmp(strings[i], strings[get(L)]) < 0) {
+                insertBefore(L, i);
+                break;
+            }
+        }
+    }
+
+    printf("\n");
+    printList(stdout, L);
+    printf("\n");
+    freeList(&L);
+
+    // Free the array of strings
+    for (int i = 0; i < n; i++) { free(strings[i]); }
+    free(strings);
+
+    // Close files
     fclose(in);
     fclose(out);
     
