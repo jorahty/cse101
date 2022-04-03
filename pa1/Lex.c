9c4005ee2b7bf9c8ad122abb3b664757f0b29720
James Tennant <jtennant@ucsc.edu>
2022-04-02 22:49:44 -0700

    implement step 1 of Lex.c sketch

--- pa1/Lex.c
+++ pa1/Lex.c
@@ -1,10 +1,17 @@
 // James Tennant • jtennant • pa1
 
 #include <stdio.h>
+#include "List.h"
 
-int main(void) {
+int main(int argc, char *argv[]) {
 
-    printf("Hello Lex!\n");
+    // 1. Check that there are two command-line arguments
+    if (argc != 3) {
+        fprintf(stderr,
+            "Error: more or less than two "
+            "command-line arguments were given.\n"
+        );
+    }
     
     return 0;
 }
