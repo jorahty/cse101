d303ef2de6b90b6affe1b84b031a74adc9f0a0f6
James Tennant <jtennant@ucsc.edu>
2022-05-03 19:55:36 -0700

    add outline

--- pa5/List.cpp
+++ pa5/List.cpp
@@ -1,3 +1,195 @@
 // James Tennant • jtennant • pa5
 
 #include "List.h"
+
+// ██ Private Constructors ██
+
+// List::Node::Node(ListElement x) {
+
+// }
+
+// // ██ Class Constructors & Destructors ██
+
+// // Creates new List in the empty state
+// List::List() {
+
+// }
+
+// // Copy constructor
+// List::List(const List& L) {
+
+// }
+
+// // Destructor
+// List::~List() {
+
+// }
+
+// // ██ Access functions ██
+
+// // Returns the length of this List
+// int List::length() const {
+
+// }
+
+// // Returns the front element in this List
+// // pre: length()>0
+// ListElement List::front() const {
+
+// }
+
+// // Returns the back element in this List
+// // pre: length()>0
+// ListElement List::back() const {
+
+// }
+
+// // Returns the position of cursor in this List: 0 <= position() <= length()
+// int List::position() const {
+
+// }
+
+// // Returns the element after the cursor
+// // pre: position()<length()
+// ListElement List::peekNext() const {
+
+// }
+
+// // Returns the element before the cursor
+// // pre: position()>0
+// ListElement List::peekPrev() const {
+
+// }
+
+// // ██ Manipulation procedures ██
+
+// // Deletes all elements in this List, setting it to the empty state
+// void List::clear() {
+
+// }
+
+// // Moves cursor to position 0 in this List
+// void List::moveFront() {
+
+// }
+
+// // Moves cursor to position length() in this List
+// void List::moveBack() {
+
+// }
+
+// // Advances cursor to next higher position. Returns the List element that
+// // was passed over.
+// // pre: position()<length()
+// ListElement List::moveNext() {
+
+// }
+
+// // Advances cursor to next lower position. Returns the List element that
+// // was passed over.
+// // pre: position()>0
+// ListElement List::movePrev() {
+
+// }
+
+// // Inserts x after cursor
+// void List::insertAfter(ListElement x) {
+
+// }
+
+// // Inserts x before cursor
+// void List::insertBefore(ListElement x) {
+
+// }
+
+// // Overwrites the List element after the cursor with x
+// // pre: position()<length()
+// void List::setAfter(ListElement x) {
+
+// }
+
+// // Overwrites the List element before the cursor with x
+// // pre: position()>0
+// void List::setBefore(ListElement x) {
+
+// }
+
+// // Deletes element after cursor
+// // pre: position()<length()
+// void List::eraseAfter() {
+
+// }
+
+// // Deletes element before cursor
+// // pre: position()>0
+// void List::eraseBefore() {
+
+// }
+
+// // ██ Other Functions ██
+
+// // Starting from the current cursor position, performs a linear search (in
+// // the direction front-to-back) for the first occurrence of element x. If x
+// // is found, places the cursor immediately after the found element, then
+// // returns the final cursor position. If x is not found, places the cursor
+// // at position length(), and returns -1
+// int List::findNext(ListElement x) {
+
+// }
+
+// // Starting from the current cursor position, performs a linear search (in
+// // the direction back-to-front) for the first occurrence of element x. If x
+// // is found, places the cursor immediately before the found element, then
+// // returns the final cursor position. If x is not found, places the cursor
+// // at position 0, and returns -1
+// int List::findPrev(ListElement x) {
+
+// }
+
+// // Removes any repeated elements in this List, leaving only unique elements.
+// // The order of the remaining elements is obtained by retaining the frontmost
+// // occurrance of each element, and removing all other occurances. The cursor
+// // is not moved with respect to the retained elements, i.e. it lies between
+// // the same two retained elements that it did before cleanup() was called
+// void List::cleanup() {
+
+// }
+
+// // Returns a new List consisting of the elements of this List, followed by
+// // the elements of L. The cursor in the returned List will be at postion 0
+// List List::concat(const List& L) const {
+
+// }
+
+// // Returns a string representation of this List consisting of a comma
+// // separated sequence of elements, surrounded by parentheses
+// std::string List::to_string() const {
+
+// }
+
+// // Returns true if and only if this List is the same integer sequence as R.
+// // The cursors in this List and in R are unchanged
+// bool List::equals(const List& R) const {
+
+// }
+
+// // ██ Overriden Operators ██
+
+// // operator<<()
+// // Inserts string representation of L into stream
+// std::ostream& operator<<(std::ostream& stream, const List& L) {
+
+// }
+
+// // operator==()
+// // Returns true if and only if A is the same integer sequence as B. The
+// // cursors in both Lists are unchanged
+// bool operator==(const List& A, const List& B) {
+
+// }
+
+// // operator=()
+// // Overwrites the state of this List with state of L
+// List& operator=(const List& L) {
+
+// }
