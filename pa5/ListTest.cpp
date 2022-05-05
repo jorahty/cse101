d5806b661ca63db72c932a3f00d110388da679c3
James Tennant <jtennant@ucsc.edu>
2022-05-04 20:02:38 -0700

    improve ListTest

--- pa5/ListTest.cpp
+++ pa5/ListTest.cpp
@@ -38,6 +38,8 @@ void printInfo(const List& L) {
 
 int main() {
 
+    cout << "\033[35m"; // Change color
+
     cout << "Hello ListTest!\n";
 
     List L;
@@ -103,70 +105,73 @@ int main() {
     cout << (R.equals(L) ? "true" : "false") << endl;
     cout << (L == R ? "true" : "false") << endl;
 
-    provided();
+    cout << "\033[0m"; // Reset color
+
+    provided(); // Run test provided in ListClient.cpp
 
     return 0;
 }
 
 void provided() {
+    cout << "\nProvided test from ListClient.cpp:\n\n";
 
-    // int i, n = 10;
-    // List A, B, C, D;
+    int i, n = 10;
+    List A, B, C, D;
 
-    // for (i = 1; i <= n; i++) {
-    //     A.insertAfter(i);
-    //     B.insertAfter(11 - i);
-    //     C.insertBefore(i);
-    //     D.insertBefore(11 - i);
-    // }
+    for (i = 1; i <= n; i++) {
+        A.insertAfter(i);
+        B.insertAfter(11 - i);
+        C.insertBefore(i);
+        D.insertBefore(11 - i);
+    }
 
-    // cout << endl;
-    // cout << "A = " << A << endl;
-    // cout << "A.position() = " << A.position() << endl;
-    // cout << "B = " << B << endl;
-    // cout << "B.position() = " << B.position() << endl;
-    // cout << "C = " << C << endl;
-    // cout << "C.position() = " << C.position() << endl;
-    // cout << "D = " << D << endl;
-    // cout << "D.position() = " << D.position() << endl;
-    // cout << endl;
+    cout << endl;
+    cout << "A = " << A << endl;
+    cout << "A.position() = " << A.position() << endl;
+    cout << "B = " << B << endl;
+    cout << "B.position() = " << B.position() << endl;
+    cout << "C = " << C << endl;
+    cout << "C.position() = " << C.position() << endl;
+    cout << "D = " << D << endl;
+    cout << "D.position() = " << D.position() << endl;
+    cout << endl;
 
-    // A.moveBack();
-    // B.moveBack();
-    // C.moveFront();
-    // D.moveFront();
+    A.moveBack();
+    B.moveBack();
+    C.moveFront();
+    D.moveFront();
 
-    // cout << "A = " << A << endl;
-    // cout << "A.position() = " << A.position() << endl;
-    // cout << "B = " << B << endl;
-    // cout << "B.position() = " << B.position() << endl;
-    // cout << "C = " << C << endl;
-    // cout << "C.position() = " << C.position() << endl;
-    // cout << "D = " << D << endl;
-    // cout << "D.position() = " << D.position() << endl;
+    cout << "A = " << A << endl;
+    cout << "A.position() = " << A.position() << endl;
+    cout << "B = " << B << endl;
+    cout << "B.position() = " << B.position() << endl;
+    cout << "C = " << C << endl;
+    cout << "C.position() = " << C.position() << endl;
+    cout << "D = " << D << endl;
+    cout << "D.position() = " << D.position() << endl;
 
-    // cout << endl
-    //      << "B: ";
-    // for (i = 1; i <= 7; i++) {
-    //     cout << B.movePrev() << " ";
-    // }
-    // cout << endl
-    //      << "B.position() = " << B.position() << endl;
+    cout << endl
+         << "B: ";
+    for (i = 1; i <= 7; i++) {
+        cout << B.movePrev() << " ";
+    }
+    cout << endl
+         << "B.position() = " << B.position() << endl;
 
-    // cout << endl
-    //      << "C: ";
-    // for (i = 1; i <= 7; i++) {
-    //     cout << C.moveNext() << " ";
-    // }
-    // cout << endl
-    //      << "C.position() = " << C.position() << endl;
+    cout << endl
+         << "C: ";
+    for (i = 1; i <= 7; i++) {
+        cout << C.moveNext() << " ";
+    }
+    cout << endl
+         << "C.position() = " << C.position() << endl;
 
-    // cout << endl;
-    // cout << "A==B is " << (A == B ? "true" : "false") << endl;
-    // cout << "B==C is " << (B == C ? "true" : "false") << endl;
-    // cout << "C==D is " << (C == D ? "true" : "false") << endl;
-    // cout << "D==A is " << (D == A ? "true" : "false") << endl;
-    // cout << endl;
+    cout << endl;
+    cout << "A==B is " << (A == B ? "true" : "false") << endl;
+    cout << "B==C is " << (B == C ? "true" : "false") << endl;
+    cout << "C==D is " << (C == D ? "true" : "false") << endl;
+    cout << "D==A is " << (D == A ? "true" : "false") << endl;
+    cout << endl;
 
     // cout << B.findNext(5) << endl;
     // cout << B << endl;
